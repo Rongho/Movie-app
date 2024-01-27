c01c9dd1d4042554b8f0b59a41ba7322e12c7ae9
Souvik Ghosh <96991068+Rongho@users.noreply.github.com>
2024-01-28 01:59:08 +0530

    Error update

--- src/pages/404/ErorrResult.jsx
+++ src/pages/404/ErorrResult.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './style.scss'
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
 
 const ErorrResult = () => {
   return (
-    <div>ErorrResult</div>
+    <div className="pageNotFound">
+      <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+    </div>
   )
 }
 
