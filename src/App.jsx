47bcd68c155d408b70ee2438cd9f5ee2740b8369
Souvik Ghosh <96991068+Rongho@users.noreply.github.com>
2024-01-28 02:49:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { useSelector,useDispatch } from 'react-redux';
 import { getApiConfiguration, getGenres } from './Store/homeslice';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Home from './pages/home/home';
-import Details from './pages/details/details';
-import SearchResult from './pages/searchResult/searchResult';
-import Explore from './pages/explore/explore';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
 import ErorrResult from '../src/pages/404/ErorrResult';
 
 function App() {
